02ffc217d4df4498e53dbb8692489d4db82525c9
Folombas <folombas@yandex.ru>
2025-11-05 22:32:29 +0300

    Added file main.go in console-game Space Defender - Digital Transformation

---
+++ day9-space-defender/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	game := NewGame()
+
+	ebiten.SetWindowSize(800, 600)
+	ebiten.SetWindowTitle("🚀 Space Defender - Digital Transformation")
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
